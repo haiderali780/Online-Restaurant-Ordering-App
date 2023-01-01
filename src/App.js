9340ae7928f678cbb2cad40600bb72bf99b63caa
haiderali780 <muhammadhaiderali782002@gmail.com>
2023-01-01 14:31:45 -0800

    adding a meals component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import {Fragment} from 'react'
-import Header from './components/Layout/Header';
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
-  <Fragment>
-  <Header>
-    
-  </Header>
-  </Fragment>
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
