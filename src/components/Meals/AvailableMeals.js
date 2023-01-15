1f5f56ac7effdab97cb176ffbd0e943c8261c850
haiderali780 <muhammadhaiderali782002@gmail.com>
2023-01-15 15:06:20 -0800

    handling error

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,18 +1,22 @@
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealsItem/MealItem";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
-
-  const [isLoading,setIsLoading]=useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://react-http-53317-default-rtdb.firebaseio.com/meals.json');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -29,30 +33,48 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
-if(isLoading)
-{
-  return(<section className={classes.loading}><p className={classes.lText}>Loading...</p></section>);
-}
+  if (isLoading) {
+    return (
+      <section className={classes.loading}>
+        <p className={classes.lText}>Loading...</p>
+      </section>
+    );
+  }
 
+  if (httpError) {
+    return (
+      <section className={classes.errormessage}>
+        <p>something went wrong</p>
+      </section>
+    );
+  }
 
-  const mealList = meals.map((meal) => (
+  const mealsList = meals.map((meal) => (
     <MealItem
-      id={meal.id}
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
     />
   ));
+
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealList}</ul>
+        <ul>{mealsList}</ul>
       </Card>
     </section>
   );
 };
+
 export default AvailableMeals;
+
+// const response = await fetch('https://react-http-53317-default-rtdb.firebaseio.com/meals.json');
